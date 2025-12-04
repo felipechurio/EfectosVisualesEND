144f31e0b8e0d93cd6ddb8eda8c3359f46bc2ee0
felipechurio <felipe.churio@davinci.edu.ar>
2025-12-04 05:56:40 -0300

    MiniFix

--- Assets/AcidCollision.cs
+++ Assets/AcidCollision.cs
@@ -14,8 +14,6 @@ public class AcidCollision : MonoBehaviour
 
     [SerializeField] Material AcidEffect;
 
-    //[SerializeField]  Material BackupMaterial;
-
 
 
     [SerializeField] GameObject Blood1;
@@ -39,15 +37,7 @@ public class AcidCollision : MonoBehaviour
 
     private void Start()
     {
-        
-
-        // acidFeatureOriginal = acidFeature;  // guardo referencia original
         acidFeature.SetActive(false);
-
-
-        //AcidIntensity = AcidEffect.GetFloat("_AcidPower");
-
-        //AcidEffect.SetFloat("_AcidIntensity", 0);
     }
 
 
@@ -65,15 +55,6 @@ public class AcidCollision : MonoBehaviour
 
             Loses = true;
 
-
-            //AcidEffect.SetFloat("_AcidIntensity", 0.5f);
-
-            //InCollison = true;
-
-            //StopAllCoroutines();
-            //StartCoroutine(LerpAcid());
-            //t = 0.5f;
-
         }
     }
 
@@ -81,8 +62,6 @@ public class AcidCollision : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            //AcidEffect = BackupMaterial;
-
             acidFeature.SetActive(true);
             AcidEffect.SetFloat("_AcidIntensity", 0.22f);
 
@@ -108,10 +87,7 @@ public class AcidCollision : MonoBehaviour
             Blood3.SetActive(false);
 
             StopCoroutine(LerpAcid());
-            InCollison = false;
-
-
-           
+            InCollison = false;         
         }
     }
     public IEnumerator LerpAcid()
@@ -125,24 +101,22 @@ public class AcidCollision : MonoBehaviour
     }
 
     public IEnumerator LoseGame()
-    {     
+    {     if (Loses)
+          {
             for (float d = 0; d < 3f; d += Time.deltaTime)
             {
                 yield return null;
-               // AcidEffect.SetFloat("_AcidIntensity", 0);
 
                 if (d > 2.9f) SceneManager.LoadScene("Level_1");
+            }
 
-            } 
+          }
+            
     }
 
    
     private void Update()
     {
         if (Loses == false) StopCoroutine(LoseGame());
-
-
-        
-
     }
 }
